73b8cb926d6c44a4f1e3bf0f8137c7a598c85b0b
Paweł <61461340+PawelRogoza@users.noreply.github.com>
2021-05-23 09:13:05 +0200

    initial test

---
+++ tests/unit/KalkulatorTest.spec.js
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils'
+import Kalkulator from '../../src/views/Kalkulator.vue';
+
+test('check if error is showing up', () => {
+    const wrapper = mount(Kalkulator)
+    
+})
